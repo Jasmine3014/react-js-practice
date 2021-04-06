353fc8942232fea666d24f37500dc2caf8abef4e
jasmine <jasminetripti5@gmail.com>
2021-04-06 13:18:53 +0600

    count ,counting by state

--- src/App.js
+++ src/App.js
@@ -7,18 +7,26 @@ class App extends Component {
   //   super(props);
   //   this.count = 5;
   // }
-  count =10;
-
+  state ={
+    count:5
+  }
   render(){
     // console.log(this.count);
 
   return (
     <div className="App">
         {/* <Practice /> */}
-        <h4>Count = {this.count}</h4>
+        <h4>Count value = {this.state.count}</h4>
+        <button onClick={() =>{
+            this.setState({count: this.state.count +1})
+            // console.log('clicked',this.state.count);
+
+
+        }
+        }>Add me</button>
+
          {/* <Profile name="Jasmine Akter" age="23" /> */}
          {/* <Profile name="Abdur Rahman" age="27"/> */}
-
     </div>
   );
 }

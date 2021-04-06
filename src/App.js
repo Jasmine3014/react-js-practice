2a70d3498e514ef92fd76958744809cc49471b81
jasmine <jasminetripti5@gmail.com>
2021-04-06 12:34:38 +0600

    count print by state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
+import { Component } from "react";
 import Practice from "./component/practice/Practice";
-// import Profile from "./component/profile";
+import Profile from "./component/profile";
 
-function App() {
+class App extends Component {
+  // constructor(props){
+  //   super(props);
+  //   this.count = 5;
+  // }
+  count =10;
+
+  render(){
+    // console.log(this.count);
+
   return (
     <div className="App">
-        <Practice />
+        {/* <Practice /> */}
+        <h4>Count = {this.count}</h4>
          {/* <Profile name="Jasmine Akter" age="23" /> */}
          {/* <Profile name="Abdur Rahman" age="27"/> */}
 
     </div>
   );
+}
 }
 
 export default App;
